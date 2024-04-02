6b40cbd9acbdc72e9d0c1100a0df2af2d6a2397b
alinutz03 <popescu.alina@79@yahoo.com>
2024-04-03 00:06:08 +0300

    Create controller, move and import the methods for the routes

--- server.js
+++ server.js
@@ -6,28 +6,15 @@ const dotenv = require('dotenv');
 dotenv.config({path : './config/config.env'});
 
 const app = express();
+
+// Rouate files
+const bootcamps = require('./routes/bootcamps')
+
+// Mount router
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-// Create route
-app.get('/api/v1/bootcamps', (req, res) =>{
-  res.status(200).json({success: true, message: 'Show all bootcamp'})
-})
-
-app.get('/api/v1/bootcamps/:id', (req, res) =>{
-  res.status(200).json({success: true, message: `Get bootcamp ${req.params.id}`})
-})
-
-app.post('/api/v1/bootcamps', (req, res) =>{
-  res.status(200).json({success: true, message: 'Show all bootcamp'})
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res) =>{
-  res.status(200).json({success: true, message: `Update bootcamp ${req.params.id}`})
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res) =>{
-  res.status(200).json({success: true, message: `Delete bootcamp ${req.params.id}`})
-})
 
 app.listen(
   PORT, 
